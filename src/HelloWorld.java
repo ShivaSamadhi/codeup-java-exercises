b4728136e6684c2265c804499731425b0f77f510
Ramaj Johnson <rjohnson3795@gmail.com>
2022-09-28 11:03:48 -0500

    syntax exercise done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,41 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Here is a string";
+        System.out.println(myString);
+
+        float myNumber = (float) 3.14;
+        System.out.println(myNumber);
+        //declaring a variable without initializing its value is fine but when we try to print that variable to the console it will throw us an error
+
+        int x = 5;
+        System.out.println(++x); // pre-increment = 6
+        //System.out.println(x++); //post-increment = 5
+        System.out.println(x); // regardless of pre or post incrementation, the second call of x will equal 6
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        //int three = (int) o;
+        int three = (int) "three";
+        //System.out.println(three);
+        //this throws an error because a string cannot be cast to an int value with this particular syntax, even tho any value can be stored within an Object
+
+        int x = 4;
+        x += 5;
+
+        int x1 = 3;
+        int y1 = 4;
+        y1 *= x1;
+
+        int x2 = 10;
+        int y2 = 2;
+        x2 /= y2;
+        y2 -= x2;
+
+        short numShort = 10000;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -44,7 +44,8 @@ public class Main {
         //first we call the Arrays class and use the .toString method to print a string that contains our array. in the case of the .toString method, each primitive type has its own version of this Arrays method. This is called 'method overloading'
         int[] numbers = {1,2,3,4,5};
         //because the original initialization syntax is a bit long, this more modern array syntax was created
-
+        //this syntax can also be used to create multidimensional arrays by simply adding another square bracket in the front (int[][]) and nesting curly braces to match ({ {...},{...} })
+        //Arrays have a fixed length, once created additional items cannot be added or removed from the array
 
 //PRIMITIVE VS REFERENCE MEMORY
         byte x = 1;
@@ -60,6 +61,47 @@ public class Main {
         //by using dot notation we can mutate x which is a member of the Point class. If we print the value of point2 to the console we can see that the change to x that was made thru point1 is visible as part of point2 => .Point[x=2, y=1]
         //REFERENCE TYPES ARE COPIED BY THEIR REFERENCES
 
+//CONSTANTS
+        final float PI = 3.14F;
+        //the keyword final allows us to create constants aka variables that are immutable. final variables should be named in all caps
+
+//ARITHMETIC EXPRESSIONS
+        //java has all the standard arithmetic expressions that we could find in JS, including the % modulus operator. The only one that is a bit more nuanced is the division operator because of the nature of how different numbers are treated within the primitive types
+
+        int expression = 10/3;
+        //Although we would normally expect this expression to be returned as a decimal, that is not the case. Because this expression is categorized as an int, the return value of 10/3 would be an integer of 3. if we wanted to get a decimal value for this expression, we would need to change up the syntax to account for that
+
+        double expression2 = (double)10/(double)3;
+
+//CASTING
+        //Casting is the processes by which values of different types are converted in order to produce a consistent result.
+        // If a value is being converted into a data type that has more available memory space then this conversion happens automatically under the hood as IMPLICIT CASTING. Example as follows
+
+        short xShort = 1;
+        //short = 2 bytes of memory
+        int yInt = xShort + 2;
+        //int = 4 bytes of memory
+
+        //because the memory space of a short can be easily stored within the lerger memory space of an int, IMPLICIT CASTING converts the types easily. Under this logic the automatic conversion chain looks like this:
+        // byte -> short -> int -> long -> float -> double
+        //Implicit casting happens when there is no loss of data between conversions. EXPLICIT CASTING is the opposite, meaning that some data will be lost during the conversion
+
+        double xDouble = 1.1;
+        int yInt2 = (int)xDouble + 2;
+        //w/o the int prefix, the return value the expression would cause an error because data would be lost if we converted a double to an integer. So we have to explicitly state that we want to convert our double value into an int value, thus giving us a return of 3 instead of the 3.1 that we might normally expect
+
+        //casting only happens between compatible types. The conversion chain we created above only works because all of those types essentially refer to some form of a number. Under this logic, a string cannot be converted to a number unless we use the appropriate wrapper class for the conversion
+
+        String xString = "1";
+        int yInt3 = Integer.parseInt(xString) + 2;
+        //there are other wrapper classes such as Short.parseShort() Float.parseFloat() and so on with a similar syntax structure
+
+
+
+
+
 
     }
 }
+
+
