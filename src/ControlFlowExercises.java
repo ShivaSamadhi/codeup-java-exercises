fdaae4c9d878748d525a4e2f774e2a6f9e20de8f
Ramaj Johnson <rjohnson3795@gmail.com>
2022-09-29 10:52:32 -0500

    do-while to for loop conversion

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -18,10 +18,19 @@ public class ControlFlowExercises {
 //            i -= 5;
 //        } while(i >= -10);
 
-        long i = 2;
-        do {
-            System.out.println(i);
-            i *= i;
-        } while(i < 1000000);
+//        for(int i = 100; i >= -10; i-= 5){
+//            System.out.println(i);
+//        };
+
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while(i < 1000000);
+
+//        for(long i = 2; i < 1000000; i *= i){
+//            System.out.println(i);
+//        };
+
     }
 }
