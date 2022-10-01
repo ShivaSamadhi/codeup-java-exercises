646e6d3fede80a5afc9884d218a5137736354d79
Ramaj Johnson <rjohnson3795@gmail.com>
2022-10-01 12:35:49 -0500

    getInteger method

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
     public static long addition(int num1, int num2){
         return num1 + num2;
@@ -17,10 +19,23 @@ public class MethodsExercises {
         return num1 / num2;
     }
 
+    public static int getInteger(int min, int max){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number between 1 and 10: ");
+        int userInput = Integer.parseInt(scanner.next());
+        if(userInput < min || userInput > max){
+            System.out.println("Enter a valid number within the range");
+        } else {
+            System.out.printf("Your number is within range");
+        }
+        return userInput;
+    }
     public static void main(String[] args) {
         System.out.println(addition(2, 3));
         System.out.println(subtraction(2, 3)); ;
         System.out.println(multiplication(2, 3));
         System.out.println(division(2, 3));
+
+        System.out.println(getInteger(1, 10));
     }
 }
