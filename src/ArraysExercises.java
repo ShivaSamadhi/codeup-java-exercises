43c4835d47fc490fbf7f210a3a284b4311be7e09
Ramaj Johnson <rjohnson3795@gmail.com>
2022-10-13 13:07:34 -0500

    arrays exercises done

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,7 +1,25 @@
+import java.util.Arrays;
+
 public class ArraysExercises {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(numbers);
+        System.out.println(Arrays.toString(numbers));
 
+        Person[] people = new Person[3];
+        people[0] = new Person("Ramaj");
+        people[1] = new Person("Anthony");
+        people[2] = new Person("Shiva");
+
+        for (Person person : people){
+            System.out.printf("%s", person.getName());
+        }
+
+
+    }
+    public static Person[] addPerson(Person[] peopleArray, Person personToAdd){
+        Person[] tempArr = Arrays.copyOf(peopleArray, peopleArray.length +1);
+        tempArr[tempArr.length-1] = personToAdd;
+
+        return tempArr;
     }
 }
