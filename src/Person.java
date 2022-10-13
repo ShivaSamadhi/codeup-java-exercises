eea443658cc03ba842bf28728791dd915e089e02
Ramaj Johnson <rjohnson3795@gmail.com>
2022-10-13 11:25:09 -0500

    person class methods

--- src/Person.java
+++ src/Person.java
@@ -4,4 +4,16 @@ private String name;
     public Person(String name) {
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void sayHello(){
+        System.out.printf("Hello, %s", name);
+    }
 }
