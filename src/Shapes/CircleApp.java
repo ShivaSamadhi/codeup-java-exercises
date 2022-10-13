384f1a7875c09ecb0868db9e92c261cbcf2c48a7
Ramaj Johnson <rjohnson3795@gmail.com>
2022-10-13 12:50:08 -0500

    circle app class completed

--- src/Shapes/CircleApp.java
+++ src/Shapes/CircleApp.java
@@ -1,4 +1,16 @@
 package Shapes;
 
+import util.Input;
+
 public class CircleApp {
+    public static void main(String[] args) {
+        System.out.println("Enter a radius:");
+        double radius = Input.getDouble();
+
+        Circle circle = new Circle(radius);
+        double area = circle.getArea();
+        double circumference = circle.getCircumference();
+
+        System.out.printf("You created a circle with a radius of %s, an area of %s, and a circumference of %s", radius, area, circumference);
+    }
 }
